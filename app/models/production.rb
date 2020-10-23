4a371364d7094f98972e33b7b814344af2268582
valdrinkuchi <v.kuqi1@gmail.com>
2020-10-23 11:16:37 +0200

    production validations

--- app/models/production.rb
+++ app/models/production.rb
@@ -2,7 +2,8 @@ class Production < ApplicationRecord
   belongs_to :order
   belongs_to :producer
   has_one :calculation, dependent: :destroy
-
+  validate :production_validate
+  before_save :assign_production
   after_save :save_calculation, if: @load_count && @load_date
 
   def save_calculation
@@ -10,6 +11,20 @@ class Production < ApplicationRecord
       Calculation.update(production: self)
     else
       Calculation.create(production: self)
+    end
+  end
+
+  def assign_production
+    order.in_production = true
+  end
+
+  private
+
+  def production_validate
+    if load_date < order.due
+      errors.add(:production, message: 'Load date cannot be same or earlier as Due date')
+    elsif price > order.price
+      errors.add(:production, message: 'Production price cannot be greater than Saling price')
     end
   end
 end
