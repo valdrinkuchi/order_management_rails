094472af2b2a791e1f828a824e63066575c3577a
Valdrin Kuchi <valdrinkuchi@gmail.com>
2020-10-28 23:05:21 +0100

    order creation issue

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,7 +1,7 @@
 class Brand
   include Mongoid::Document
   belongs_to :customer
-  has_many :order
+  has_many :orders
   validates :name, presence: true, uniqueness: true
   field :name, type: String
 end
